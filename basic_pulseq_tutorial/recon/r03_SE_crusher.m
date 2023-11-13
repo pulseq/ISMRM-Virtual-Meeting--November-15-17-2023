2048db9a13145b25bef6c1b051820db971f69ff4
Qingping Chen <qingping_chen@hotmail.com>
2023-11-13 17:44:46 +0100

    Add files via upload

---
+++ basic_pulseq_tutorial/recon/r03_SE_crusher.m
@@ -0,0 +1,99 @@
+clear all; close all; clc;
+% very basic FID data handling example
+%
+% needs mapVBVD in the path
+
+%% Load data from the given directory sorted by name
+path = pwd ; % directory to be scanned for data files
+nF = 1 ; % the number of the file / data set to load
+
+pattern = '*s03*.dat' ;
+D = dir([path filesep pattern]) ;
+[~,I] = sort(string({D(:).name})) ;
+data_file_path = [path filesep D(I(nF)).name] ;
+disp(['loading `' data_file_path '´ ...']) ;
+twix_obj = mapVBVD(data_file_path) ;
+
+%% raw data preparation
+if iscell(twix_obj)
+    data_unsorted = double(twix_obj{end}.image.unsorted()) ;
+else
+    data_unsorted = double(twix_obj.image.unsorted()) ;
+end
+
+channels = size(data_unsorted, 2) ;
+adc_len = size(data_unsorted, 1) ;
+readouts = size(data_unsorted, 3) ;
+
+rawdata = permute(data_unsorted, [1,3,2]) ;
+
+%% Load sequence from the file with the same name as the raw data
+seq_file_path = 's03_se_crusher.seq' ;
+fprintf(['loading `' seq_file_path '´ ...\n']) ;
+seq = mr.Sequence() ;              % Create a new sequence object
+if nF~=2 % 'detectRFuse' will not fork for multi-FA FID sequence because alpha excitations with the flip-angle larger than 90 degrees will be detected as refocusing pulses
+    seq.read(seq_file_path,'detectRFuse') ;
+else
+    seq.read(seq_file_path) ;
+end
+
+%% we just want t_adc
+[ktraj_adc, t_adc, ktraj, t, t_excitation, t_refocusing] = seq.calculateKspacePP() ;
+
+t_adc = reshape(t_adc,[adc_len,readouts]) ;
+% calc TE but account for dummy scans...
+%[~,iND]=max(t_excitation(t_excitation<t_adc(1,1)));
+%t_e=t_adc-t_excitation(ones(1,adc_len),iND:end);
+t_relevant_excitation = zeros(1,readouts) ;
+for i = 1:readouts
+    t_relevant_excitation(i) = max(t_excitation(t_excitation<t_adc(1,i))) ;
+end
+t_e = t_adc-t_relevant_excitation(ones(1,adc_len),:) ;
+
+%% plot raw data
+figure; plot(t_e, abs(rawdata(:,:,1))); title('k-space data');
+hold on;
+if channels>1
+    for j=2:channels
+        plot(t_e, abs(rawdata(:,:,j))) ;
+    end
+end
+ 
+plot(t_e(1), 0) ; % trick to force Y axis scaling
+xlabel('time since excitation /s');
+exportgraphics(gcf,'s03_kspace.png',...
+    'ContentType','vector',...
+    'BackgroundColor','none') ;
+
+if (readouts>1)
+    figure; plot(abs(rawdata(4,:,1))); title('time evolution (4th FID point)');
+    if channels>1
+        hold on;
+        for j=2:channels
+            plot(abs(rawdata(4,:,j)));
+        end
+    end
+
+    dataavg=squeeze(mean(rawdata,2));
+    figure; plot(t_e, abs(dataavg(:,1))); title('averaged FIDs');
+    if channels>1
+        hold on;
+        for j=2:channels
+            plot(t_e, abs(dataavg(:,2)));
+        end
+    end
+    xlabel('time since excitation /s');
+end
+
+%% plot spectr(um/a)
+data_fft = fftshift(fft(fftshift(rawdata,1)), 1) ;
+w_axis = (-adc_len/2:(adc_len/2-1))/((t_adc(end,1,1)-t_adc(1,1,1))/(adc_len-1)*adc_len)/twix_obj{2}.hdr.Dicom.lFrequency*1e6' ;
+figure ;
+plot(w_axis, abs(data_fft(:,:,1)), 'lineWidth', 1) ;
+title('abs spectr(um/a)') ;
+xlim([-10 10]) ; xlabel('frequency /ppm') ;
+set(gca,'Xdir','reverse') ;
+
+exportgraphics(gcf,'s03_im.png',...
+    'ContentType','vector',...
+    'BackgroundColor','none') ;
---
+++ basic_pulseq_tutorial/recon/r12_GRE2D_optimizedSpoiler.m
@@ -0,0 +1,131 @@
+% Reconstruction of 2D Cartesian Pulseq data
+% provides an example on how data reordering can be detected from the MR
+% sequence with almost no additional prior knowledge
+%
+% needs mapVBVD in the path
+clear all;close all;clc
+%% Load data sorted by name
+path = pwd ; % directory to be scanned for data files
+nF = 1 ; % the number of the file / data set to load
+
+pattern='*s12*.dat';
+D=dir([path filesep pattern]) ;
+[~,I]=sort(string({D(:).name})) ;
+data_file_path=[path filesep D(I(nF)).name] ;
+
+%% Load the file from a dir
+twix_obj = mapVBVD(data_file_path);
+
+%% Load sequence from file (optional, you may still have it in memory)
+seq_file_path = 's12_gre_spoiler.seq';
+
+[~,out_name,~] = fileparts(seq_file_path); 
+
+seq = mr.Sequence() ;              % Create a new sequence object
+seq.read(seq_file_path,'detectRFuse') ;
+
+[ktraj_adc, t_adc, ktraj, t_ktraj, t_excitation, t_refocusing] = seq.calculateKspacePP();
+figure; plot(ktraj(1,:),ktraj(2,:),'b',...
+             ktraj_adc(1,:),ktraj_adc(2,:),'r.'); % a 2D plot
+axis('equal');
+title('k-space');
+xlabel('kx / m^-^1') ; 
+ylabel('kx / m^-^1') ; 
+exportgraphics(gcf,'s12_k-space.png',...
+    'ContentType','vector',...
+    'BackgroundColor','none') ;
+
+%% Analyze the trajectory data (ktraj_adc)
+k_extent = max(abs(ktraj_adc),[],2) ;
+k_scale = max(k_extent) ;
+k_threshold = k_scale/5000 ;
+
+% detect unused dimensions and delete them
+if any(k_extent<k_threshold)
+    ktraj_adc(k_extent<k_threshold,:)=[]; % delete rows
+    k_extent(k_extent<k_threshold)=[];
+end
+
+% detect dK, k-space reordering and repetitions (or slices, etc)
+kt_sorted=sort(ktraj_adc,2);
+dk_all=kt_sorted(:,2:end)-kt_sorted(:,1:(end-1));
+dk_all(dk_all<k_threshold)=NaN;
+dk_min=min(dk_all,[],2);
+dk_max=max(dk_all,[],2);
+dk_all(dk_all-dk_min(:,ones(1,size(dk_all,2)))>k_threshold)=NaN;
+dk_all_cnt=sum(isfinite(dk_all),2);
+dk_all(~isfinite(dk_all))=0;
+dk=sum(dk_all,2)./dk_all_cnt;
+[~,k0_ind]=min(sum(ktraj_adc.^2,1));
+kindex=round((ktraj_adc-ktraj_adc(:,k0_ind*ones(1,size(ktraj_adc,2))))./dk(:,ones(1,size(ktraj_adc,2))));
+kindex_min=min(kindex,[],2);
+kindex_mat=kindex-kindex_min(:,ones(1,size(ktraj_adc,2)))+1;
+kindex_end=max(kindex_mat,[],2);
+sampler=zeros(kindex_end');
+repeat=zeros(1,size(ktraj_adc,2));
+for i=1:size(kindex_mat,2)
+    if (size(kindex_mat,1)==3)
+        ind=sub2ind(kindex_end,kindex_mat(1,i),kindex_mat(2,i),kindex_mat(3,i));
+    else
+        ind=sub2ind(kindex_end,kindex_mat(1,i),kindex_mat(2,i)); 
+    end
+    repeat(i)=sampler(ind);
+    sampler(ind)=repeat(i)+1;
+end
+if (max(repeat(:))>0)
+    kindex=[kindex;(repeat+1)];
+    kindex_mat=[kindex_mat;(repeat+1)];
+    kindex_end=max(kindex_mat,[],2);
+end
+
+%% sort in the k-space data
+if iscell(twix_obj)
+    data_unsorted = twix_obj{end}.image.unsorted();
+else
+    data_unsorted = twix_obj.image.unsorted();
+end
+
+% the incoming data order is [kx coils acquisitions]
+data_coils_last = permute(data_unsorted, [1, 3, 2]);
+nCoils = size(data_coils_last, 3);
+
+data_coils_last=reshape(data_coils_last, [size(data_coils_last,1)*size(data_coils_last,2), nCoils]) ;
+
+data = zeros([kindex_end' nCoils]) ;
+if (size(kindex,1)==3)
+    for i=1:size(kindex,2)
+        data(kindex_mat(1,i),kindex_mat(2,i),kindex_mat(3,i),:) = data_coils_last(i,:) ;
+    end
+else
+    for i=1:size(kindex,2)
+        data(kindex_mat(1,i),kindex_mat(2,i),:) = data_coils_last(i,:) ;
+    end
+end
+
+if size(kindex,1) == 3
+    nImages = size(data,3) ;
+else
+    nImages = 1 ;
+    data=reshape(data, [size(data,1) size(data,2) 1 size(data,3)]) ;
+end
+
+%% Reconstruct coil images
+
+images = zeros(size(data)) ;
+for ii = 1:nCoils
+    images(:,:,:,ii) = fftshift(fft2(fftshift(data(end:-1:1,:,:,ii)))) ;
+end
+
+%% Image display with optional sum of squares combination
+figure ;
+if nCoils>1
+    sos=abs(sum(images.*conj(images),ndims(images))).^(1/2) ;
+    sos=sos./max(sos(:)) ;    
+    imshow(rot90(sos),[]) ;
+else
+    imab(abs(images));
+end
+colormap('default') ;
+exportgraphics(gcf,'s12_im.png',...
+    'ContentType','vector',...
+    'BackgroundColor','none') ;

2b9b33f0b34cb880f7360d45f11844f1b2704efc
Qingping Chen <qingping_chen@hotmail.com>
2023-11-13 14:25:10 +0100

    Update s12_GRE2D_acceleratedComputation.m

--- basic_pulseq_tutorial/sequence/s12_GRE2D_acceleratedComputation.m
+++ basic_pulseq_tutorial/sequence/s12_GRE2D_acceleratedComputation.m
@@ -1,9 +1,12 @@
 %% ISMRM virtual meeting 15.11.2023
-% Build a 2D GRE sequence with optimized spoiler and accelerated computation
+% Build a 2D GRE sequence with optimized spoiler and accelerated
+% computation
+clear all; close all; clc ;
+
 %% set system limits and parameters
 sys = mr.opts('MaxGrad', 22, 'GradUnit', 'mT/m', ...
     'MaxSlew', 120, 'SlewUnit', 'T/m/s', ...
-    'rfRingdownTime', 20e-6, 'rfDeadTime', 100e-6, 'adcDeadTime', 10e-6) ;
+    'rfRingdownTime', 20e-6, 'rfDeadTime', 100e-6, 'adcDeadTime', 20e-6) ;
 
 seq = mr.Sequence(sys) ;           % Create a new sequence object
 fov = 256e-3 ; Nx = 256 ; Ny = 256 ;     % Define FOV and resolution
@@ -38,23 +41,22 @@ end
 % gradient spoiling
 gxSpoil = mr.makeExtendedTrapezoidArea(gx.channel, gx.amplitude, 0, 2*Nx*deltak, sys) ;
 gxSpoil.delay = mr.calcDuration(gx1) ;
-gzSpoil.delay = max(mr.calcDuration(gx1),gzSpoil.delay) ;
+gx_add = mr.addGradients({gx1, gxSpoil},'system',sys) ;
+
+gzSpoil = mr.makeTrapezoid('z','Area',4/sliceThickness,'system',sys) ;
+gzSpoil.delay = max(mr.calcDuration(gx1), gzSpoil.delay) ;
+
 for iY = 1:Ny
     gyReph(iY).delay = max(mr.calcDuration(gx1), gyReph(iY).delay) ;
 end
-
-gzSpoil = mr.makeTrapezoid('z','Area',4/sliceThickness,'system',sys) ;
-
-gx1 = mr.addGradients({gx1, gxSpoil},'system',sys) ;
-
 
 %% Calculate timing
 delayTE = ceil((TE - mr.calcDuration(gxPre) - gz.fallTime - gz.flatTime/2 ...
     - mr.calcDuration(gx)/2)/seq.gradRasterTime) * seq.gradRasterTime ;
 delayTR = ceil((TR - mr.calcDuration(gz) - mr.calcDuration(gxPre) ...
-    - mr.calcDuration(gx1,gzSpoil,gyReph) - delayTE)/seq.gradRasterTime)*seq.gradRasterTime ;
-assert(delayTE >= 0) ;
-assert(delayTR >= 0) ;
+    - mr.calcDuration(gx_add, gzSpoil, gyReph) - delayTE)/seq.gradRasterTime)*seq.gradRasterTime ;
+assert(delayTE >= 0 ) ;
+assert(delayTR >= 0 ) ;
 
 %% accelerate computations
 % preregister constant objects to accelerate computations
@@ -70,8 +72,8 @@ for iY = 1:Ny
     gyReph(iY).id = seq.registerGradEvent(gyReph(iY)) ;
 end
 
-tic ;
 %% Loop over phase encodes and define sequence blocks
+tic ;
 for i=1:Ny
     % RF spoiling (vary RF phase pseudo-randomly)
     rand_phase = mod(117*(i^2 + i + 2), 360) * pi/180 ;
@@ -81,10 +83,11 @@ for i=1:Ny
     seq.addBlock(rf, gz) ;
     seq.addBlock(gxPre, gyPre(i), gzReph) ;
     seq.addBlock(mr.makeDelay(delayTE)) ;
-    seq.addBlock(gx1, adc, gzSpoil, gyReph(i) ) ;
+    seq.addBlock(gx_add, adc, gzSpoil, gyReph(i) ) ;
     seq.addBlock(mr.makeDelay(delayTR) ) ;
 end
 toc ;
+
 %% check whether the timing of the sequence is correct
 [ok, error_report]=seq.checkTiming;
 
@@ -98,9 +101,9 @@ end
 
 %% prepare sequence export
 seq.setDefinition('FOV', [fov fov sliceThickness]);
-seq.setDefinition('Name', 'gre');
+seq.setDefinition('Name', 'grecomp');
 
-seq.write('gre.seq')       % Write to pulseq file
+seq.write('grecomp.seq')       % Write to pulseq file
 
 %% plot sequence and k-space diagrams
 
